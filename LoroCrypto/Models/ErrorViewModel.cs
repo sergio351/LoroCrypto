8cdf9918ce145f696f4bc00cf88ab594725ac7cd
sergio351 <velazquezsergio714@gmail.com>
2022-09-22 21:09:02 -0300

    first commit

---
+++ LoroCrypto/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LoroCrypto.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
